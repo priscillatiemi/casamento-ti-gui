2a015e548c79a8f19974b337386056c09aa36f13
Priscilla <tiemiakemi@hotmail.com>
2026-09-21 23:47:29 -0300

    ajustando texto da lista de presentes

--- src/components/Gifts.jsx
+++ src/components/Gifts.jsx
@@ -375,19 +375,20 @@ export default function Gifts() {
 
       <h2>Lista de presentes</h2>
 
+      <p>
+        Queridos familiares e amigos, para quem quiser nos presentear,
+        deixamos como contribuição preferencial o PIX.
+        Ficaremos muito gratos!
+      </p>
+
       <p className="sectionIntro">
         Os valores são apenas sugestões.
         Você pode presentear via PIX ou cartão de crédito.
       </p>
 
       <p>
-        Queridos familiares e amigos, para quem quiser nos presentear,
-        deixamos como contribuição preferencial o PIX.
-        Ficaremos muito gratos! Agradecemos demais pelo carinho e apoio!
-      </p>
-
-      <p>
-        Chave PIX: tiemiakemi@hotmail.com
+        Chave PIX: tiemiakemi@hotmail.com <br />
+        Agradecemos demais pelo carinho e apoio!
       </p>
 
       <div className="giftGrid">
